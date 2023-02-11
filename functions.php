1011a494d2e0c50bf91b93b8a8c9665694b31949
Olaf <Tkaczyk68@gmai.com>
2023-02-11 17:19:20 +0100

    Adding dynamic single page content

--- functions.php
+++ functions.php
@@ -8,5 +8,8 @@ function university_files(){
     wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
 
 }
-
+function university_features(){
+    add_theme_support('title-tag');  
+}
 add_action('wp_enqueue_scripts', 'university_files');
+add_action('after_setup_theme', 'university_features');
